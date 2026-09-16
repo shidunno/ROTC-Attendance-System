e8cac99a8014d4364b9dd19357ab155079cae486
shidunno <justinesalcedo2006@gmail.com>
2026-09-17 01:23:31 +0800

    Fix composer build dependencies and lock file for production

--- api/index.php
+++ api/index.php
@@ -1,4 +1,9 @@
 <?php
 
-// Forward Vercel requests to Laravel's public/index.php
-require __DIR__ . '/../public/index.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->handleRequest(
+    \Illuminate\Http\Request::capture()
+);
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @viteReactRefresh
-    @vite('resources/js/app.jsx')
+    @vite('resources/js/App.jsx')
     @inertiaHead
     <title>Document</title>
 </head>
